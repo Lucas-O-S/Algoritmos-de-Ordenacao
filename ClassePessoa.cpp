a8d3b0c8d1b734f964f3d08bede2e81bab00c4f5
Lucas Oliveira Silva <127321390+Lucas-O-S@users.noreply.github.com>
2024-03-08 13:54:31 -0300

    Update ClassePessoa.cpp
    Construtor tirado e pequenas mudanças

--- ClassePessoa.cpp
+++ ClassePessoa.cpp
@@ -3,48 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 #include <string>
 #include <chrono>
 
 using namespace std;
 
-int CalcularIdade(string dataNascimento) {
-
-	//Cria um variavel de stringstreams para separar a data de nascimento  de uma string
-	istringstream ss(dataNascimento);
-
-	//Variaveis para cada um
-	int dia, mes, ano;
-
-	//Variavel de delimitador
-	char delimitador;
-
-	// Adiciona cada parte da data de nascimento em cada variavel separando pelo delimitador
-	ss >> dia >> delimitador >> mes >> delimitador >> ano;
-
-	//Cria uma variavel de um ponto no tempo que representa hoje
-	auto hoje = chrono::system_clock::now();
-
-	//Converte o ponto para quantos segundos desde 1970
-	time_t TempoHoje = chrono::system_clock::to_time_t(hoje);
-
-	//Converte o tempo para representar o tempo local
-	tm* hojeLocal = localtime(&TempoHoje);
-
-	// Calcula a idade
-	//Soma 1900 por causa que ele calcula os anos a partir de 1900 então falta 1900 anos se não fazer a soma
-	int idade = hojeLocal->tm_year + 1900 - ano;
-
-	//Verifica se a pessoa já fez aniversario no ano
-	//Atravez do mês atual mais 1 ou do mes atual e o dia atual
-	if (mes > hojeLocal->tm_mon + 1 || (mes == hojeLocal->tm_mon + 1 && dia > hojeLocal->tm_mday)) {
-		idade--;
-	}
-
-	
-	return idade;
-}
 
 
 
@@ -56,13 +19,49 @@ class Pessoa {
 		float peso, altura;
 
 		
+		int CalcularIdade(string dataNascimento) {
+
+			//Cria um variavel de stringstreams para separar a data de nascimento  de uma string
+			istringstream ss(dataNascimento);
+
+			//Variaveis para cada um
+			int dia, mes, ano;
+
+			//Variavel de delimitador
+			char delimitador;
+
+			// Adiciona cada parte da data de nascimento em cada variavel separando pelo delimitador
+			ss >> dia >> delimitador >> mes >> delimitador >> ano;
+
+			//Cria uma variavel de um ponto no tempo que representa hoje
+			auto hoje = chrono::system_clock::now();
+
+			//Converte o ponto para quantos segundos desde 1970
+			time_t TempoHoje = chrono::system_clock::to_time_t(hoje);
+
+			//Converte o tempo para representar o tempo local
+			tm* hojeLocal = localtime(&TempoHoje);
+
+			// Calcula a idade
+			//Soma 1900 por causa que ele calcula os anos a partir de 1900 então falta 1900 anos se não fazer a soma
+			int idade = hojeLocal->tm_year + 1900 - ano;
+
+			//Verifica se a pessoa já fez aniversario no ano
+			//Atravez do mês atual mais 1 ou do mes atual e o dia atual
+			if (mes > hojeLocal->tm_mon + 1 || (mes == hojeLocal->tm_mon + 1 && dia > hojeLocal->tm_mday)) {
+				idade--;
+			}
+
+			
+			return idade;
+		}
 	;
 
 	public:
 
 
-		//Construtor
-		Pessoa(string nome, string dataNascimento, float peso, float altura) {
+		//adiciona valores ao objeto
+		void AddValoresPessoa(string nome, string dataNascimento, float peso, float altura) {
 			this->nome = nome;
 			this-> dataNascimento = dataNascimento;
 			this->idade = CalcularIdade(dataNascimento);
